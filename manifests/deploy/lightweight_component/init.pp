58566f0adb57f9d40b7a52ff35ea1f7b147e1bca
ejr004 <acessoej@icloud.com>
2018-11-29 16:54:08 +0100

    lynt code

--- manifests/deploy/lightweight_component/init.pp
+++ manifests/deploy/lightweight_component/init.pp
@@ -1,10 +1,9 @@
 class simple_grid::deploy::lightweight_component::init(
-  
 ){
   include 'simple_grid::ccm_function::create_config_dir'
-  include 'simple_grid::ccm_function::create_config_dir' 
+  include 'simple_grid::ccm_function::copy'
 
- Class['simple_grid::ccm_function::create_config_dir'] -> Class['simple_grid::ccm_function::copy']
+Class['simple_grid::ccm_function::create_config_dir'] -> Class['simple_grid::ccm_function::copy']
 /*   class {'simple_grid::pre_config::create_config_dir':}
   class { 'simple_grid::pre_config::lightweight_component::ssh_config': }
   class {'simple_grid::pre_config::lightweight_component::reset_agent':}
