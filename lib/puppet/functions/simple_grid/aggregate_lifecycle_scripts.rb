fe50a6b8fc3a825b4956dd4a2a20954e48786c05
ejr004 <acessoej@icloud.com>
2018-12-04 23:26:34 +0100

    fixing array.nil (site-level-config)

--- lib/puppet/functions/simple_grid/aggregate_lifecycle_scripts.rb
+++ lib/puppet/functions/simple_grid/aggregate_lifecycle_scripts.rb
@@ -1,26 +1,24 @@
 #!/opt/puppetlabs/puppet/bin/ruby
 require 'yaml'
 
-site_level_config_file_path = "/etc/simple_grid/simple_grid_site_config/site-level-configuration-file.yaml"
-# 
+site_level_config_file_path = "/Users/gongolo/dev/puppet/simple_grid_puppet_dev_env/config_master/data/simple_grid/simple_grid_site_config/site-level-configuration-file.yaml"
+
 def get_lifecycle_scripts(site_level_config_file_path)
+        simple_grid_site_script_dir = "/etc/simple_grid/scripts/"
         lightweight_components = Hash.new
         newhash = Hash.new
         lc_hook = Hash.new
+        lc_array = Array.new
         data = YAML.load_file(site_level_config_file_path)
         lightweight_components = data["lightweight_components"]
         lightweight_components.each do |lc|
-               #puts "/etc/simple_grid/scripts/"+lc["execution_id"]
-               #newhash = lc["execution_id"]
-               lc_hook = lc["lifecycle_hooks"]
-                puts lc_hook
-                        lc_hook.each do | key, hook|
-                        puts key
-                        #puts dir exec_id 
-               end
-          end
-end
-def copy_lifecycle_scripts(exec_id,lc_hook)
+                #puts "/etc/simple_grid/scripts/"+lc["execution_id"]
+                exec_id = lc["execution_id"]
+                lc_hook = lc["lifecycle_hooks"]
+                lc_hook.each do | key,value| 
+                        puts "mkdir -p "+"#{simple_grid_site_script_dir}"+"#{exec_id}/"+"#{key}"
+                end
+        end
 end
 begin
 get_lifecycle_scripts(site_level_config_file_path)
