18b36b086f85d6ea8ebd0103e776027af9544cb0
ejr004 <acessoej@icloud.com>
2018-12-05 15:36:20 +0100

    Adding puppet function

--- lib/puppet/functions/simple_grid/aggregate_lifecycle_scripts.rb
+++ lib/puppet/functions/simple_grid/aggregate_lifecycle_scripts.rb
@@ -1,21 +1,18 @@
-#!/opt/puppetlabs/puppet/bin/ruby
 require 'yaml'
-
-site_level_config_file_path = "/Users/gongolo/dev/puppet/simple_grid_puppet_dev_env/config_master/data/simple_grid/simple_grid_site_config/site-level-configuration-file.yaml"
-
-def get_lifecycle_scripts(site_level_config_file_path)
-        simple_grid_site_script_dir = "/etc/simple_grid/scripts/"
-        lightweight_components = Hash.new
-        exec_id = Hash.new
-        data = YAML.load_file(site_level_config_file_path)
-        lightweight_components = data["lightweight_components"]
-        lightweight_components.each do |key, lc|
-                #exec_id = {key["execution_id"]=>key["lifecycle_hooks"]}
-                exec_id.store(key["execution_id"],key["lifecycle_hooks"])
-                #puts exec_id.inspect
+Puppet::Functions.create_function(:'simple_grid::aggregate_lifecycle_scripts') do
+        dispatch :get_lifecycle_scripts do
+                param 'String', :site_level_config_file_path
         end
-        return exec_id
-end
-begin
-puts get_lifecycle_scripts(site_level_config_file_path)
+        def get_lifecycle_scripts(site_level_config_file_path)
+                simple_grid_site_script_dir = "/etc/simple_grid/scripts/"
+                lightweight_components = Hash.new
+                exec_id = Hash.new
+                data = YAML.load_file(site_level_config_file_path)
+                lightweight_components = data["lightweight_components"]
+                lightweight_components.each do |key, lc|
+                        exec_id.store(key["execution_id"],key["lifecycle_hooks"])
+                        #puts exec_id.inspect
+                end
+                return exec_id
+        end
 end
